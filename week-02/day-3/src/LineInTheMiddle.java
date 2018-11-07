b903e62269ecef370d8e9245755b33d8eee95b6a
Lili <liliszendrei@gmail.com>
2018-11-07 17:17:08 +0100

    nov 7

--- week-02/day-3/src/LineInTheMiddle.java
+++ week-02/day-3/src/LineInTheMiddle.java
@@ -4,48 +4,40 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class LineInTheMiddle {
-    public static void mainDraw(Graphics graphics){
-        // draw a red horizontal line to the canvas' middle.
-        // draw a green vertical line to the canvas' middle.
+    public class LineInTheMiddle {
+        public static void mainDraw(Graphics graphics){
+            // draw a red horizontal line to the canvas' middle.
+            // draw a green vertical line to the canvas' middle.
 
-        graphics.setColor(Color.YELLOW);
-        graphics.drawLine(80, 160, 240, 160);
-        graphics.setColor(new Color (0, 0, 0, 60));
-        graphics.drawRect(100, 100, 200, 200);
+            graphics.setColor(Color.RED);
+            graphics.drawLine (0, 160, 320, 160);
 
-        graphics.fillRect(400,500,100,100);
-        graphics.setColor(Color.ORANGE);
-        int xpoints[] = {25,145,25,145,25};
-        int ypoints[] = {25,25,145,145,25};
-        int npoints = 5;
-        graphics.drawPolygon(xpoints,ypoints,npoints);
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine (160, 0, 160, 320);
 
-        for (int i = 0; i < 60; i+=30) {
-            graphics.fillRect(i, i, 20, 20);
+
+        }
+
+        // Don't touch the code below
+        static int WIDTH = 320;
+        static int HEIGHT = 320;
+
+        public static void main(String[] args) {
+            JFrame jFrame = new JFrame("Drawing");
+            jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            ImagePanel panel = new ImagePanel();
+            panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+            jFrame.add(panel);
+            jFrame.setLocationRelativeTo(null);
+            jFrame.setVisible(true);
+            jFrame.pack();
+        }
+
+        static class ImagePanel extends JPanel {
+            @Override
+            protected void paintComponent(Graphics graphics) {
+                super.paintComponent(graphics);
+                mainDraw(graphics);
+            }
         }
     }
-
-    // Don't touch the code below
-    static int WIDTH = 800;
-    static int HEIGHT = 800;
-
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ImagePanel panel = new ImagePanel();
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.add(panel);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-        jFrame.pack();
-    }
-
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-        }
-    }
-}
---
+++ week-02/day-3/src/PositionSquare.java
@@ -0,0 +1,55 @@
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+    public static void mainDraw(Graphics graphics){
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+        int pos = 20;
+        int size = 50;
+        for (int i = 0; i < 3; i++) {
+            rajzolj(pos, size, graphics);
+            pos += 20;
+        }
+
+    }
+
+public static void rajzolj (int pos, int size, Graphics graphics ){
+
+    graphics.drawRect(pos, pos, size, size);
+
+}
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+
+}
