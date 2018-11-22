253abaa734c5f4adf8282d83da8abb559e2fe34a
Lili <liliszendrei@gmail.com>
2018-11-22 17:10:33 +0100

    fixed countLetters and done fibonacci

--- week-04/day-4/src/countletters/countLetters.java
+++ week-04/day-4/src/countletters/countLetters.java
@@ -1,18 +1,17 @@
 package countletters;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class countLetters {
 
     public Map<Character, Integer> letterCounter(String str) {
 
-        Map<Character, Integer>dictionary = Collections.<Character, Integer>emptyMap();
+        Map<Character, Integer>dictionary = new HashMap<>();
         for (int i = 0; i < str.length(); i++) {
             if (dictionary.containsKey(str.charAt(i))) {
-                int counter = dictionary.get(i);
+                int counter = dictionary.get(str.charAt(i));
                 dictionary.put(str.charAt(i), counter+1);
-
             }else {
                 dictionary.put(str.charAt(i), 1);
             }
--- week-04/day-4/src/countletters/countLettersTest.java
+++ week-04/day-4/src/countletters/countLettersTest.java
@@ -3,6 +3,10 @@ package countletters;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class countLettersTest {
@@ -11,10 +15,17 @@ class countLettersTest {
 
     @BeforeEach
     void setUp() {
-        
+        object = new countLetters();
     }
 
     @Test
     void letterCounter() {
+
+        Map<Character, Integer> dict = new HashMap<>();
+        dict.put('a', 1);
+        dict.put('p', 2);
+        dict.put('l', 1);
+        dict.put('e', 1);
+        assertEquals(dict, object.letterCounter("apple"));
     }
 }
